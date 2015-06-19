665b8e9e17ec9b101c8ad7123e95067b3c9a9494
Mike Rohland <mrohland@users.noreply.github.com>
2015-06-19 12:22:20 +0200

    updated to latest atom; fixed error in build.sh for CI; updated readme

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-ATOM_VERSION='v0.206.0'
+ATOM_VERSION='v0.210.0'
+WORKSPACE="$(pwd)"
 run_in_docker() {
   docker run --rm -v $WORKSPACE:$WORKSPACE --env=WORKSPACE=$WORKSPACE mrohland/atom:$ATOM_VERSION /sbin/my_init -- $@
 }
